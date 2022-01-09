3ccbe4ca3fbf247c2c0dc3b4334d8eefc8c38560
naitik2009 <89187483+naitik2009@users.noreply.github.com>
2022-01-09 12:58:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+status = "";
+
+function preload()
+{
+   img = loadImage("BRO.jpg");
+}
+
+function setup ()
+{
+   canvas = createCanvas(640, 420);
+   canvas.center();
+   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+   document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+   console.log("Model Loaded!");
+   status = true;
+   objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+   if(error){
+      console.log(error);
+   }
+      console.log(results);
+} 
